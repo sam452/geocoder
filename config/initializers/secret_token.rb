9bd25215d4e6177899da88ebb84a1342e418fe52
sam walton <sam@foofrancine.com>
2013-06-08 20:02:31 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocoder::Application.config.secret_token = '3e446b1c5074eaa34c99309d3dcca24ee8153624b7d107c0edd9e2327b9ef2427308e693e491f069af3d41419ecb81a19fda20b741b9f0afee97993f5abb76f1'
